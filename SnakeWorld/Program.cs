56d9becf5d3f4a319d9b088f921ae8b4b45a1a7d
sadouIbnBinta <sadouibnmouhamad512@gmail.com>
2024-12-19 12:08:05 +0100

    Modification du fichier xslt qui génére la page Html contenant le rang des joueurs selon le score

--- SnakeWorld/Program.cs
+++ SnakeWorld/Program.cs
@@ -29,13 +29,14 @@ namespace SerpentJeu
             joueurs.Joueurs.Add(j3);
             
             joueurs.SerialiserJoueurs(pathXml);
-            */
+            
             SerialisableJoueurs joueursDeserialiser = new SerialisableJoueurs();
             joueursDeserialiser.DeserialiserJoueurs(pathXml);
             Console.WriteLine(joueursDeserialiser.ToString());
             joueursDeserialiser.Joueurs[0].updateMeilleurScore(89);
             Console.WriteLine(joueursDeserialiser.ToString());
-            /*try
+            */
+            try
             {
                 using var game = new JeuPrincipal();
                 game.Run();
@@ -47,7 +48,7 @@ namespace SerpentJeu
 
             // Garder la fenêtre ouverte
             Console.WriteLine("Appuyez sur une touche pour fermer...");
-            Console.ReadLine();*/
+            Console.ReadLine();
         }
     }
 }
